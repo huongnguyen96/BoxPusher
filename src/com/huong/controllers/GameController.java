464c863e1543037070bc7c7976da1a32be8b3288
huongnguyen96 <huongnguyen96@gmail.com>
2016-06-21 21:56:12 +0700

    add GameController: initGame

--- src/com/huong/controllers/GameController.java
+++ src/com/huong/controllers/GameController.java
@@ -1,5 +1,36 @@
 package com.huong.controllers;
 
+import com.huong.models.GameObject;
+import com.huong.models.Location;
+import com.huong.models.Map;
+import com.huong.models.MoveableGameObject;
+
 public class GameController {
+	private Map map;
+	private MoveableGameObject player;
+	private MoveableGameObjectController playerController;
+	private MoveableGameObject box;
+	private MoveableGameObjectController boxController;
+	private GameObject storage;
+	public void play(){
+		initGame();
+	}
+	
+	private void initGame(){
+		initGameObject();
+		map = new Map();
+		map.initScene(player.getLocation(), box.getLocation(), storage.getLocation());
+	}
+	
+	private void initGameObject(){
+		player = new MoveableGameObject();
+		player.setLocation(new Location(1,1));
+		playerController = new MoveableGameObjectController(player);
+		box = new MoveableGameObject();
+		box.setLocation(new Location(2,2));
+		boxController = new MoveableGameObjectController(box);
+		storage = new MoveableGameObject();
+		storage.setLocation(new Location(3,2));
+	}
 
 }
--- src/com/huong/controllers/MoveableGameObjectController.java
+++ src/com/huong/controllers/MoveableGameObjectController.java
@@ -1,5 +1,44 @@
 package com.huong.controllers;
 
+import com.huong.models.MoveableGameObject;
+
 public class MoveableGameObjectController {
+	public static final int LEFT = 1;
+	public static final int RIGHT = 2;
+	public static final int DOWN = 3;
+	public static final int UP = 4;
+	private MoveableGameObject moveableGameObject;
+
+	public MoveableGameObjectController(MoveableGameObject moveableGameObject) {
+		this.moveableGameObject = moveableGameObject;
+	}
+
+	public MoveableGameObject getMoveableGameObject() {
+		return moveableGameObject;
+	}
+
+	public void setMoveableGameObject(MoveableGameObject moveableGameObject) {
+		this.moveableGameObject = moveableGameObject;
+	}
+
+	public void move(int orientation) {
+		switch (orientation) {
+		case LEFT:
+			moveableGameObject.moveLeft();
+			break;
+		case RIGHT:
+			moveableGameObject.moveRight();
+			break;
+		case DOWN:
+			moveableGameObject.moveDown();
+			break;
+		case UP:
+			moveableGameObject.moveUp();
+			break;
+		default:
+			System.out.println("cannot detect orientation");
+			break;
+		}
+	}
 
 }
